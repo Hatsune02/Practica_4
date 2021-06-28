7f0fb028732b06a11f602196f069f73a3289b0a7
Dónavin Tzunún López <tzunundonavin@gmail.com>
2021-06-28 17:45:00 -0600

    AL FIN SE MUEVE LA FICHA, agregando los turnos y demas a la partida actual

--- src/com/navi/Visual/visualTablero/VisualTablero.java
+++ src/com/navi/Visual/visualTablero/VisualTablero.java
@@ -1,5 +1,6 @@
 package com.navi.Visual.visualTablero;
 
+import com.navi.Visual.Pintar;
 import com.navi.jugador.ArregloPlayer;
 import com.navi.tablero.Tablero;
 
@@ -9,22 +10,24 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class VisualTablero extends JFrame implements ActionListener{
+public class VisualTablero extends JFrame implements ActionListener, Pintar {
     public static Tablero tablero;
     public static VisualCasillas[][] casillasVisuales;
     int x = 0;
     int y = 0;
-    int numeroCasillas = 1;
     JPanel panelNorte = new JPanel(new GridLayout());
     JPanel panelSur = new JPanel();
     JPanel panelEste = new JPanel();
     JPanel panelOeste = new JPanel();
     JPanel panelTablero = new JPanel();
-    JLabel dado1 = new JLabel();
-    JLabel dado2 = new JLabel();
     static JLabel tiempo = new JLabel("", SwingConstants.RIGHT);
-    JTextField informacion = new JTextField("", SwingConstants.CENTER);
+    public static JTextArea informacion = new JTextArea();
+    public static JTextArea turno = new JTextArea();
+    JScrollPane scrollPane = new JScrollPane();
     JButton tirarDados = new JButton("Tirar Dados");
+    JPanel panelTirarDados = new JPanel();
+    JPanel panelTurno = new JPanel();
+    JPanel panelInformacion = new JPanel();
 
     public void marco(){
         this.setSize(1000,600);
@@ -38,38 +41,73 @@ public class VisualTablero extends JFrame implements ActionListener{
 
         y = tablero.getY() - 1;
         casillasVisuales = new VisualCasillas[tablero.getY()][tablero.getX()];
-        panelSur.setLayout(new GridLayout());
-        informacion.setPreferredSize(new Dimension(200, 50));
+        scrollPane.setBounds(50, 30, 300, 50);
+        panelSur.setLayout(new GridLayout(1,3));
+        informacion.setPreferredSize(new Dimension(280, 80));
+        turno.setPreferredSize(new Dimension(120, 20));
         tablero.correrTiempo();
         tiempo.setText("Tiempo de Partida: " + tablero.tiempoActual + "    ");
         tiempo.setForeground(Color.WHITE);
-        PintarBoton(tirarDados);
+        pintarBoton(tirarDados);
         tirarDados.setPreferredSize(new Dimension(150,35));
         tablero.casillaAvanzar(0,0,3);
-
+        //panelTablero.setPreferredSize(new Dimension(700, 2000));
+        scrollPane.setViewportView(panelTablero);
         panelTablero.setLayout(new GridLayout(tablero.getY(),tablero.getX()));
+        panelTirarDados.add(tirarDados);
+        panelTurno.add(turno);
+        panelInformacion.add(informacion);
+
         for (int i = 0; i < (tablero.getX()*tablero.getY()); i++) {
             ponerCasilla();
         }
-        ArregloPlayer.jugadores[0].setActual(casillasVisuales[0][0]);
-        ArregloPlayer.jugadores[0].colocarJugador(casillasVisuales[0][0]);
+
+        agregarJugadores();
+        iniciarTurnos();
+        mostrarTurnoActual();
 
         panelNorte.add(tiempo);
-        panelSur.add(tirarDados);
-        panelSur.add(informacion);
+        panelSur.add(panelInformacion);
+        panelSur.add(panelTirarDados);
+        panelSur.add(panelTurno);
         panelNorte.setBackground(Color.GRAY);
         panelSur.setBackground(Color.GRAY);
         panelEste.setBackground(Color.GRAY);
         panelOeste.setBackground(Color.GRAY);
         panelTablero.setBackground(Color.GRAY);
+        panelTirarDados.setBackground(Color.GRAY);
+        panelTurno.setBackground(Color.GRAY);
+        panelInformacion.setBackground(Color.GRAY);
 
-        this.getContentPane().add(panelTablero, BorderLayout.CENTER);
+        this.getContentPane().add(scrollPane, BorderLayout.CENTER);
         this.getContentPane().add(panelNorte, BorderLayout.NORTH);
         this.getContentPane().add(panelSur, BorderLayout.SOUTH);
         this.getContentPane().add(panelEste, BorderLayout.EAST);
         this.getContentPane().add(panelOeste, BorderLayout.WEST);
 
         tirarDados.addActionListener(this);
+
+    }
+
+    public void iniciarTurnos(){
+        int random = generaNumeroR(1,ArregloPlayer.cantidadPlayersPartida);
+        ArregloPlayer.jugadoresPartida[random - 1].turno=true;
+    }
+
+    public void mostrarTurnoActual(){
+        for (int i = 0; i < ArregloPlayer.cantidadPlayersPartida; i++) {
+            if(ArregloPlayer.jugadoresPartida[i].turno){
+                turno.setText("Turno de " + ArregloPlayer.jugadoresPartida[i].getNOMBRE()+"\n");
+                break;
+            }
+        }
+    }
+
+    public int generaNumeroR(int minimo, int maximo) {
+
+        int num = (int) Math.floor(Math.random() * (maximo - minimo + 1) + (minimo));
+        return num;
+
     }
 
     private void ponerCasilla(){
@@ -84,7 +122,15 @@ public class VisualTablero extends JFrame implements ActionListener{
         }
     }
 
-    public void PintarBoton(JButton boton){
+    private void agregarJugadores(){
+        for (int i = 0; i < ArregloPlayer.cantidadPlayersPartida; i++) {
+            ArregloPlayer.jugadoresPartida[i].setActual(casillasVisuales[0][0]);
+            ArregloPlayer.jugadoresPartida[i].colocarJugador(casillasVisuales[0][0]);
+        }
+
+    }
+
+    public void pintarBoton(JButton boton){
         boton.setBackground(Color.BLACK);
         boton.setForeground(Color.WHITE);
     }
@@ -94,19 +140,29 @@ public class VisualTablero extends JFrame implements ActionListener{
         SwingUtilities.updateComponentTreeUI(tiempo);
     }
 
-    public int generaNumeroR(int minimo, int maximo) {
-
-        int num = (int) Math.floor(Math.random() * (maximo - minimo + 1) + (minimo));
-        return num;
-
-    }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == tirarDados){
-            int n = generaNumeroR(1, 6);
-            informacion.setText("El numero del dado es " + n);
+            informacion.setText("");
+
+            for (int i = 0; i < ArregloPlayer.cantidadPlayersPartida; i++) {
+                if(ArregloPlayer.jugadoresPartida[i].turno){
+                    ArregloPlayer.jugadoresPartida[i].turnoJugador(informacion);
+                    if(i+1 == ArregloPlayer.cantidadPlayersPartida){
+                        ArregloPlayer.jugadoresPartida[i].turno = false;
+                        ArregloPlayer.jugadoresPartida[0].turno = true;
+                    }
+                    else{
+                        ArregloPlayer.jugadoresPartida[i].turno = false;
+                        ArregloPlayer.jugadoresPartida[i+1].turno = true;
+                    }
+                    break;
+                }
+            }
+            mostrarTurnoActual();
+            SwingUtilities.updateComponentTreeUI(this);
         }
     }
 
