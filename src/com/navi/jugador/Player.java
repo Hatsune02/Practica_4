977051cb49506f9634192e103c319b2a15e7be78
Dónavin Tzunún López <tzunundonavin@gmail.com>
2021-06-28 17:42:32 -0600

    Agegando metodos para mover la ficha a la clase Player, y creando un arreglo para guardar a los jugadores de cada partida

--- src/com/navi/jugador/Player.java
+++ src/com/navi/jugador/Player.java
@@ -1,12 +1,18 @@
 package com.navi.jugador;
 
+import com.navi.Visual.Pintar;
 import com.navi.Visual.visualTablero.VisualCasillas;
+import com.navi.Visual.visualTablero.VisualTablero;
+import com.navi.tablero.casillas.*;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.Serializable;
 
-public class Player implements Serializable {
+public class Player implements Serializable, Pintar, ActionListener {
     private final int ID;
     private final String NOMBRE;
     private final String APELLIDO;
@@ -15,14 +21,17 @@ public class Player implements Serializable {
     private int partidasLose = 0;
     public VisualCasillas casillaActual;
     JButton botonPlayer = new JButton();
-    public boolean turno = true;
+    public boolean turno = false;
+    public int turnoPerdido = 0;
 
     public Player(int id, String nombre, String apellido){
         this.ID = id;
         this.NOMBRE = nombre;
         this.APELLIDO = apellido;
-        //botonPlayer.setPreferredSize(new Dimension(20,20));
-        botonPlayer.setText(nombre.substring(0,2));
+        botonPlayer.setPreferredSize(new Dimension(25,25));
+        botonPlayer.setText(nombre.substring(0,1));
+        pintarBoton(botonPlayer);
+        botonPlayer.addActionListener(this);
     }
 
     public void colocarJugador(VisualCasillas actual){
@@ -30,16 +39,61 @@ public class Player implements Serializable {
         casillaActual = actual;
         casillaActual.panelCentral.add(botonPlayer);
         casillaActual.referencia.accion(this);
-        /*
-        if(turno){
-            tirarDados();
-        }
-        else{
-            turno = true;
-        }
-
-         */
     }
+
+    public void turnoJugador(JTextArea informacion){
+        if(turnoPerdido == 0){
+            tirarDados(informacion);
+        }
+    }
+
+    public void tirarDados(JTextArea informacion){
+        int numeroDado = generaNumeroR(1,6);
+        boolean encontrado = false;
+        Casilla[][] tablero = VisualTablero.tablero.getCasillas();
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                if(tablero[i][j].getNumeroCasilla() == casillaActual.referencia.getNumeroCasilla() + numeroDado){
+                    colocarJugador(VisualTablero.casillasVisuales[i][j]);
+                    String info = informacion.getText();
+                    String tipoCasilla = compararCasillas(casillaActual.referencia);
+                    informacion.setText(info + "El número del dado es " + numeroDado + "\n" + NOMBRE +" se movio a la casilla: " + casillaActual.referencia.getNumeroCasilla() + "\n" + tipoCasilla);
+                    encontrado = true;
+                    break;
+                }
+            }
+            if(encontrado){
+                break;
+            }
+        }
+    }
+
+    public String compararCasillas(Casilla casilla){
+        String a = "";
+        if(casilla instanceof CasillaNula){
+            a = "Casilla Normal: no ocurre nada...";
+        }
+        else if(casilla instanceof Avanza){
+            a = "Casilla Avanzar: avanzas " + ((Avanza) casilla).posicion + " casillas";
+        }
+        else if(casilla instanceof Retrocede){
+            a = "Casilla Retroceder: retrocedes " + ((Retrocede) casilla).posicion + " casillas";
+        }
+        else if(casilla instanceof PierdeTurno){
+            a = "Pierde Turno: Perdiste un turno en la siguiente vuelta";
+        }
+        else if(casilla instanceof TiraDados){
+            a = "Tira Dados: puedes volver a tirar los Dados";
+        }
+        else if(casilla instanceof Subida){
+            a = "Casilla Subir: Subiste hacia la casilla " + casilla.getNumeroCasilla();
+        }
+        else if(casilla instanceof Bajada){
+            a = "Casilla Bajar: Bajaste hacia la casilla " + casilla.getNumeroCasilla();
+        }
+        return a;
+    }
+
 
     public void setActual(VisualCasillas actual) {
         this.casillaActual = actual;
@@ -79,5 +133,56 @@ public class Player implements Serializable {
 
     public int getPartidasLose() {
         return partidasLose;
+    }
+
+    public int generaNumeroR(int minimo, int maximo) {
+
+        int num = (int) Math.floor(Math.random() * (maximo - minimo + 1) + (minimo));
+        return num;
+
+    }
+
+    @Override
+    public void pintarBoton(JButton boton) {
+        boton.setBorder(new LineBorder(Color.BLACK));
+        if(ID == 1){
+            boton.setBackground(Color.RED);
+            boton.setForeground(Color.WHITE);
+        }
+        else if(ID == 2){
+            boton.setBackground(Color.BLUE);
+            boton.setForeground(Color.WHITE);
+        }
+        else if(ID == 3){
+            boton.setBackground(Color.YELLOW);
+            boton.setForeground(Color.WHITE);
+        }
+        else if(ID == 4){
+            boton.setBackground(Color.MAGENTA);
+            boton.setForeground(Color.WHITE);
+        }
+        else if(ID == 5){
+            boton.setBackground(Color.ORANGE);
+            boton.setForeground(Color.WHITE);
+        }
+        else if(ID == 6){
+            boton.setBackground(Color.GREEN);
+            boton.setForeground(Color.WHITE);
+        }
+        else if(ID == 7){
+            boton.setBackground(Color.GRAY);
+            boton.setForeground(Color.WHITE);
+        }
+        else if(ID == 8){
+            boton.setBackground(Color.BLACK);
+            boton.setForeground(Color.WHITE);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == botonPlayer){
+            JOptionPane.showMessageDialog(null, "Jugador id " + ID +"\nNombre " + NOMBRE + "\nApellidos " + APELLIDO + "\nCasilla Actual " + casillaActual.referencia.getNumeroCasilla());
+        }
     }
 }
